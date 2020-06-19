d3ac8ddc693aab3ae36c229e1a9bb409d47a6b16
xNaznara <kanlandball553@gmail.com>
2020-06-19 16:51:16 +0300

    moved calculations to server

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(cors())
 
+app.use(bodyParser.json())
+
 const arr = [
-  'click me', 'click click click', 'hey! it hirts!'
+  'clicker', 'click tester', 'test your cps'
 ]
 
-app.get('/button-name', (req, res) => {
+app.get('/title', (req, res) => {
   let random = Math.random() * 2
   let rounded = Math.round(random)
   res.send(arr[rounded])
+})
+
+app.post('/calculate', (req, res) => {
+  try{
+    if(req.body.action === '$') {
+      let calculated = eval(req.body.expression)
+      let rounded = Math.round(calculated)
+      res.send({rounded: rounded})
+    } else if(req.body.action === '=') {
+        let calculated = eval(req.body.expression)
+        res.send({calculated: calculated})
+    } else if(req.body.action === 'arrow') {
+        let backspace = req.body.expression.slice(0, -1)
+        res.send({backspace: backspace})
+    }
+  } catch (e) {
+    res.send({error: e})
+  }
 })
 
 app.listen(3000, ()=>console.log("Started"))
